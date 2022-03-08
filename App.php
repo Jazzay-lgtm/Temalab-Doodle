1ae2ffd180f0bb2ca7584365ad59be41fdf046d7
EosRicsi <mEGLESZARPOG4RE>
2022-03-08 17:27:44 +0100

    Név Regex, teszt kell majd hozzá 3 részre szedtem, index,app,style

--- App.php
+++ App.php
@@ -2,32 +2,27 @@
 $name=$_POST["Nev"];
 $email=$_POST["Email"];
 $regex=new Regex();
-$regex->name_regex($name);
-$regex->email_regex($email);
+if($regex->name_regex($name)==true &&$regex->email_regex($email)){
+    echo "Sikeres foglalás!";
+}
 class Regex
 {
     public function email_regex($e){
         if(filter_var($e, FILTER_VALIDATE_EMAIL)) {
             //Valid email!
-            echo "Az e-mail jó";
-        }
-        else if(empty($n)){
-            echo "Kérlek írd be az e-mail címet";
+            return true;
         }
         else{
-            echo "Az e-mail nem jó";
+            return false;
         }
 
     }
     public function name_regex($n){
         if (preg_match("/^([A-ZÁÉÚŐÓÜÖÍ]([a-záéúőóüöí.]+\s?)){2,}$/", $n)) {
-            echo "A név jó";
-        }
-        else if(empty($n)){
-            echo "Kérlek írd be a neved!";
+            return true;
         }
         else {
-            echo 'A név nem jó';
+            return false;
         }
 
     }
