190b8902da6dc03e9cc0c57f4e6fdd6ce351145d
EosRicsi <mEGLESZARPOG4RE>
2022-03-12 20:02:49 +0100

    input dizajn

---
+++ App.php
@@ -0,0 +1,34 @@
+<?php
+$name=$_POST["Nev"];
+$email=$_POST["Email"];
+$regex=new Regex();
+if($regex->name_regex($name)==true && $regex->email_regex($email)){
+    echo "Sikeres foglalás!";
+}
+else if($regex->name_regex($name)==false || $regex->email_regex($email)==false){
+    echo "Sikertelen foglalás!";
+}
+class Regex
+{
+    public function email_regex($e){
+        if(filter_var($e, FILTER_VALIDATE_EMAIL)) {
+            //Valid email!
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    public function name_regex($n){
+        if (preg_match("/^([A-ZÁÉÚŐÓÜÖÍ]([a-záéúőóüöí.]+\s?)){2,}$/", $n)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+}
+
