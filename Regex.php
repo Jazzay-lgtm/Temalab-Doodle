149b2d06c4240e7332ef95ff63c3e5433fbc4bef
penzes98 <32952776+penzes98@users.noreply.github.com>
2022-03-23 18:52:04 +0100

    Add files via upload

---
+++ Regex.php
@@ -0,0 +1,35 @@
+<?php
+$name=$_POST["Nev"];
+$email=$_POST["Email"];
+$regex=new Regex();
+if($regex->name_regex($name)==true && $regex->email_regex($email)){
+    echo "Sikeres foglalás!";
+}
+else if($regex->name_regex($name)==false || $regex->email_regex($email)==false){
+    echo "Sikertelen foglalás!";
+}
+class Regex
+{
+    public function email_regex($e): bool
+    {
+        if(filter_var($e, FILTER_VALIDATE_EMAIL)) {
+            //Valid email!
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    public function name_regex($n): bool
+    {
+        if (preg_match("/^[A-Za-z\x{00C0}-\x{00FF}][A-Za-z\x{00C0}-\x{00FF}\'\-]+([\ A-Za-z\x{00C0}-\x{00FF}][A-Za-z\x{00C0}-\x{00FF}\'\-]+)*/u", $n)) {
+            return true;
+        }
+        else {
+            return false;
+        }
+
+    }
+
+}
