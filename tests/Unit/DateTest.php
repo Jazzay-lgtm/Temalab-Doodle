33b02b5613197bd920775d8dd97ad29a5d33ea00
Gizella Takács <takacs.gizella0303@gmial.com>
2022-03-30 00:23:50 +0200

    Regex.php-ben benne van mostmár, hogy a dátumot is ellenőrrizze Dates validates hiba javítás

---
+++ tests/Unit/DateTest.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class DateTest extends TestCase
+{
+    public function test_date_later_now_date_01(){
+        $this->assertFalse([\DateValidates::class,'startDateLaterNowDate'('2020-03-07 11:00')]);
+    }
+    public function test_date_later_now_date_02(){
+        $startDate = time();
+        $dateFuture = date('Y-m-d H:i', strtotime('+1 day', $startDate));
+        $this->assertTrue([\DateValidates::class,'startDateLaterNowDate'($dateFuture)]);
+    }
+}
