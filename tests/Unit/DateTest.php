57fff5177ca9feea89c7c1b647884baf420d2c24
Gizella Takács <takacs.gizella0303@gmial.com>
2022-03-30 11:00:46 +0200

    Test hibák javítása

--- tests/Unit/DateTest.php
+++ tests/Unit/DateTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Unit;
 
+use DateValidates;
 use PHPUnit\Framework\TestCase;
 
 require 'DatesValidates.php';
@@ -9,11 +10,13 @@ require 'DatesValidates.php';
 class DateTest extends TestCase
 {
     public function test_date_later_now_date_01(){
-        $this->assertFalse([\DateValidates::class,'startDateLaterNowDate'('2020-03-07 11:00')]);
+        $dates = new DateValidates();
+        $this->assertEquals(false,$dates->startDateLaterNowDate('2020-03-07 11:00'),"rossz idópont");
     }
     public function test_date_later_now_date_02(){
         $startDate = time();
         $dateFuture = date('Y-m-d H:i', strtotime('+1 day', $startDate));
-        $this->assertTrue([\DateValidates::class,'startDateLaterNowDate'($dateFuture)]);
+        $dates = new DateValidates();
+        $this->assertEquals(true,$dates->startDateLaterNowDate($dateFuture),"jó időpont");
     }
 }
